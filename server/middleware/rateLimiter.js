ba838eff7c5d1311ecdee68785376201c1ea0b15
Suyan Man Amatya <srkamatya007@gmail.com>
2026-04-21 14:29:00 +0545

    Database connect

--- server/middleware/rateLimiter.js
+++
@@ -1,7 +0,0 @@
-const rateLimit = require("express-rate-limit");
-const globalLimiter = rateLimit({ windowMs: 15*60*1000, max: 100, standardHeaders: true, legacyHeaders: false });
-const submitLimiter = rateLimit({
-  windowMs: 10*60*1000, max: 5, standardHeaders: true, legacyHeaders: false,
-  message: { message: "Too many submissions. Please wait before trying again." },
-});
-module.exports = { globalLimiter, submitLimiter };
--- server/routes/resultRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { submitResult, getAllResults } = require("../controllers/resultController");
-const { submitLimiter } = require("../middleware/rateLimiter");
-router.post("/submit", submitLimiter, submitResult);
-router.get("/", getAllResults);
-module.exports = router;
